def1731ef4188599435a1fe1bd24c172a27dac59
DIAN SWASTIANI <i16025@ftis>
2018-11-02 14:19:09 +0700

    calculate

--- rpl-git/Calculator.java
+++ rpl-git/Calculator.java
@@ -1,22 +1,26 @@
 public classs Calculator
 {
+	public Calculator()
+	{
+		
+	}
 	public float plus(float a,float b)
 	{
-		return a+b;
+		
 	}
 	
 	public float minus(float a,float b)
 	{
-		return a-b;
+		
 	}
 	
 	public float multiply(float a,float b)
 	{
-		return a*b;
+		
 	}
 	
 	public float division(float a,float b)
 	{
-		return a/b;
+		
 	}
 }
